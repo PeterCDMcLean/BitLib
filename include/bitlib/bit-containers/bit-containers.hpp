2e365587911aba3f61fa0a2ecc48b9bb11d9e356
Peter McLean <peter.cd.mclean@gmail.com>
2025-05-10 14:11:47 -0400

    Move literal to its own file

--- include/bitlib/bit-containers/bit-containers.hpp
+++ include/bitlib/bit-containers/bit-containers.hpp
@@ -9,6 +9,7 @@
 // ============================== PREAMBLE ================================== //
 #include "bit_array.hpp"
 #include "bit_array_dynamic_extent.hpp"
+#include "bit_literal.hpp"
 #include "bit_mdspan_accessor.hpp"
 #include "bit_span.hpp"
 #include "bit_vector.hpp"
--- include/bitlib/bit-containers/bit_array.hpp
+++ include/bitlib/bit-containers/bit_array.hpp
@@ -10,8 +10,6 @@
 #define _BIT_ARRAY_HPP_INCLUDED
 // ========================================================================== //
 
-
-
 // ================================ PREAMBLE ================================ //
 // C++ standard library
 #include <algorithm>
@@ -55,16 +53,17 @@ template <typename T = bit_value,
           typename W = std::conditional_t<std::is_same_v<T, bit_value>, uint8_t, T>>
 class bit_array : public bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>> {
  public:
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::word_type;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::value_type;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::size_type;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::difference_type;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::reference;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::const_reference;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::pointer;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::const_pointer;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::iterator;
-  using typename bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>::const_iterator;
+  using base = bit_array_base<bit_array<T, N, V, W>, T, W, detail::bit_array_d_it<T, W, N>, detail::bit_array_d_cit<T, W, N>>;
+  using typename base::const_iterator;
+  using typename base::const_pointer;
+  using typename base::const_reference;
+  using typename base::difference_type;
+  using typename base::iterator;
+  using typename base::pointer;
+  using typename base::reference;
+  using typename base::size_type;
+  using typename base::value_type;
+  using typename base::word_type;
 
   static constexpr std::size_t bits = N * bitsof<T>();
 
@@ -334,101 +333,6 @@ constexpr bit_array<T, N, V, W>::operator U() const noexcept
   return result;
 }
 
-// ========================================================================== //
-template <char Bit>
-constexpr void _parameter_pack_base_bits_prefix_len(size_t& base, size_t& bits, size_t& prefix_len, uint64_t& num) {
-  if ('\'' == Bit) {
-    if (0 != bits) {
-      return;  //skip. Should we, though? It seems like it will be confusing
-    }
-    bits = num;
-    num = 0;
-    ++prefix_len;
-    if (0 == base) {
-      base = 10;
-    }
-  } else if (0 == base) {
-    if (Bit == 'b' || Bit == 'B') {
-      base = 2;
-      num = 0;
-    } else if (Bit == 'x' || Bit == 'X') {
-      base = 16;
-      num = 0;
-    } else if (Bit == 'd' || Bit == 'D') {
-      base = 10;
-      num = 0;
-    } else {
-      num = (num * 10) + (Bit - '0');
-    }
-    ++prefix_len;
-  } else {
-    uint8_t decimal;
-    switch (base) {
-      case 2:
-        decimal = static_cast<uint8_t>(Bit - '0');
-        break;
-      case 10:
-        decimal = static_cast<uint8_t>(Bit - '0');
-        break;
-      case 16:
-        decimal = static_cast<uint8_t>(Bit - '0');
-        if (Bit >= 'a' && Bit <= 'f') {
-          decimal = static_cast<uint8_t>(Bit - 'a') + 10u;
-        }
-        if (Bit >= 'A' && Bit <= 'F') {
-          decimal = static_cast<uint8_t>(Bit - 'A') + 10u;
-        }
-        break;
-    }
-    num = num * base + decimal;
-  }
-}
-
-template <char Bit, char... Bits>
-constexpr void _parameter_pack_base_bits_prefix_len(size_t& base, size_t& bits, size_t& prefix_len, uint64_t& num)
-  requires(sizeof...(Bits) > 0)
-{
-  _parameter_pack_base_bits_prefix_len<Bit>(base, bits, prefix_len, num);
-  _parameter_pack_base_bits_prefix_len<Bits...>(base, bits, prefix_len, num);
-}
-
-template <char... Bits>
-constexpr std::pair<size_t, uint64_t> _parameter_pack_decode_prefixed_num() {
-  size_t base = 0;
-  size_t bits = 0;
-  size_t prefix_len = 0;
-  uint64_t num = 0;
-  _parameter_pack_base_bits_prefix_len<Bits...>(base, bits, prefix_len, num);
-  size_t digits = (sizeof...(Bits) - prefix_len);
-  if (0 == base) {
-    base = 10;
-    digits = prefix_len;
-    prefix_len = 0;
-  }
-  if (0 == bits) {
-    if (base == 2) {
-      bits = digits;
-    } else if (base == 10) {
-      bits = std::bit_width(num);
-    } else {
-      bits = 4 * digits;
-    }
-  }
-  return {bits, num};
-}
-
 } // namespace bit
 
-template <char... Str>
-constexpr bit::bit_array<bit::bit_value, bit::_parameter_pack_decode_prefixed_num<Str...>().first> operator""_b() {
-  bit::bit_array<bit::bit_value, bit::_parameter_pack_decode_prefixed_num<Str...>().first> arr{};
-  auto [bits, num] = bit::_parameter_pack_decode_prefixed_num<Str...>();
-  for (int i = 0; i < arr.size(); i++) {
-    arr[i] = (num & 0x1) ? bit::bit1 : bit::bit0;
-    num >>= 1;
-  }
-  return arr;
-}
-
 #endif // _BIT_ARRAY_HPP_INCLUDED
-       // ========================================================================== //
